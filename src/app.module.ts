b3d162dbc361bc58f74bea4b8df013451b023f69
rizkydipahiyat <rizkydipahiyat76@gmail.com>
2023-10-08 15:04:13 +0700

    update setting cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import * as express from 'express';
+import { Express } from 'express'
 
 @Module({
   imports: [
@@ -19,4 +21,28 @@ import { ProductModule } from './product/product.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: any) {
+    const app: Express = consumer.getHttpAdapter().getInstance();
+
+    // Enable CORS with the desired options
+    app.use(
+      express.json(), // Optional: Enable JSON parsing middleware
+      express.urlencoded({ extended: true }), // Optional: Enable URL-encoded body parsing middleware
+    );
+    app.use((req, res, next) => {
+      const allowedOrigins = ['http://localhost:5173']; // List of allowed origins
+
+      if (allowedOrigins.includes(req.headers.origin)) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin); // Set the allowed origin dynamically
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept',
+        );
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      }
+
+      next();
+    });
+  }
+}
